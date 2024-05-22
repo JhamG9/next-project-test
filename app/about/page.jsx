1767f641e7634c65130ed0207714f2715b7207b4
Jham Giron <jhampiergiron@gmail.com}>
2024-05-22 17:43:04 -0500

    feat: added basic redirections in next.js

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutApp = () => {
+  return (
+    <h1>AboutApp</h1>
+  )
+}
+
+export default AboutApp
---
+++ app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Computadoras = () => {
+  return (
+    <div>
+        <h1>Computadoras</h1>
+    </div>
+  )
+}
+
+export default Computadoras;
