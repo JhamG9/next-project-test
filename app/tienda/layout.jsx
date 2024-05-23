3948bc97754069659e642ecfd88ffddc3256b79b
Jham Giron <jhampiergiron@gmail.com}>
2024-05-22 21:25:31 -0500

    feat: added page and logic with layout

---
+++ app/tienda/layout.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const TiendaLayout = ({ children }) => {
+  return (
+    <>
+      <nav>
+        <h3>Seccion Tienda</h3>
+
+        <ul>
+          <li>
+            <Link href="/tienda/categorias">Categorias</Link>
+          </li>
+          <li>
+            <Link href="/tienda/categorias/computadoras">Computadoras</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+};
+
+export default TiendaLayout;
