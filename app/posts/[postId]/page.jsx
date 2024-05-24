eb9a43d093f23d2fd6d8ece9eecb1c866d15679e
Jham Giron <jhampiergiron@gmail.com}>
2024-05-24 00:56:49 -0500

    feat: added Suspense for fetch data

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Posts from "../page";
 
-const loadPost = async(id) =>{
+const loadPost = async (id) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   const data = await res.json();
   return data;
-}
+};
 
-const PostPage = async({ params }) => {
+const PostPage = async ({ params }) => {
   const post = await loadPost(params.postId);
-  
+
   return (
     <div>
-      <h1>{post.id} {post.title}</h1>
-      <p>{post.body}</p>      
+      <h1>
+        {post.id} {post.title}
+      </h1>
+      <p>{post.body}</p>
+
+      <hr />
+
+      <h3>Otras Publicaciones</h3>
+
+      {/* Sirve para cargar datos de manera independiente sin afectar a los demas componentes */}
+      <Suspense fallback={<div>Cargando publicaciones....</div>}>
+        <Posts />
+      </Suspense>
     </div>
   );
 };
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 const loadPosts = async() =>{
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await res.json();
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     return data;
 }
 
 // React Server Component
-const PostPages = async () => {
+const Posts = async () => {
     const posts = await loadPosts();
     return (
     <div>
@@ -21,4 +24,4 @@ const PostPages = async () => {
   )
 }
 
-export default PostPages
+export default Posts;
